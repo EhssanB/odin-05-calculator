905b09f5152aa0f15375b781b9670df1545c3ec3
EhssanB <169449063+EhssanB@users.noreply.github.com>
2024-12-14 22:27:07 +0000

    add logic to store inputs from DOM

--- script.js
+++ script.js
@@ -10,10 +10,31 @@
 // 4. calculationResult
 
 let intiateStateObj = () => {
+    let add = (x, y) => {
+        return x + y
+    }
+    
+    let subtract = (x,y) => {
+        return x - y
+    }
+    
+    let divide = (x,y) => {
+        return x / y
+    }
+    
+    let multiply = (x,y) => {
+        return x * y
+    }
+    
     return {
         selectedNumbers: [],
         selectedOperation: null,
         calculationResult: 0,
+        isAfterOperation: false,
+        add,
+        subtract,
+        divide,
+        multiply
     }
 } 
 
@@ -32,7 +53,7 @@ let storeNumber = (calcObj, number) => {
 // 1b. selectOperation
 
 let storeOperation = (calcObj, operation) => {
-    calcObj.selectedOperation = operation
+    calcObj.selectedOperation = calcObj[operation];
 }
 
 // 2. calculateOperation
@@ -43,7 +64,10 @@ let calculateOperation = calcObj => {
 
     let operation = calcObj.selectedOperation;
 
-    calcObj.calculationResult = operation(x, y)
+    let result = operation(x,y);
+
+    calcObj.calculationResult = result
+    calcObj.selectedNumbers = [result];
 }
 
 // 2a. add
@@ -51,27 +75,12 @@ let calculateOperation = calcObj => {
 // 2c. divide
 // 2d. multiply
 
-let add = (x, y) => {
-    return x + y
-}
-
-let subtract = (x,y) => {
-    return x - y
-}
-
-let divide = (x,y) => {
-    return x / y
-}
-
-let multiply = (x,y) => {
-    return x * y
-}
-
 // DOM Logic
 
 // DOM variables
 
 let calcDisplay = document.querySelector(".display");
+
 let numberNodeList = document.querySelectorAll(".number");
 let operatorNodeList = document.querySelectorAll(".operator");
 
@@ -80,6 +89,12 @@ let clearBtn = document.querySelector(".clear");
 // Displaying Numbers
 
 let displayInputNumber = e => {
+
+    if(calcStateObj.isAfterOperation) {
+        console.log("Number input has come after selecting operation")
+        clearDisplay();
+        calcStateObj.isAfterOperation = false;
+    }
 
     if (e.type == "click") {
         calcDisplay.textContent += e.target.textContent;
@@ -93,8 +108,12 @@ let displayInputNumber = e => {
 
 let numberKeyInput = e => {
 
+    if (e.code == "Period" && calcDisplay.textContent.includes(".")) return
+
     let numberString = "1234567890.";
     if (numberString.includes(e.key)) displayInputNumber(e);
+
+    console.log(e)
     
 }
 
@@ -119,7 +138,7 @@ let backSpaceDelete = (e) => {
 numberNodeList.forEach(
 
     numberDiv => numberDiv.addEventListener("click", displayInputNumber)
-    
+
 );
 
 addEventListener("keydown", e => {
@@ -130,3 +149,39 @@ addEventListener("keydown", e => {
 })
 
 clearBtn.addEventListener("click", clearDisplay);
+
+// Operators:
+
+
+
+let storeInput = e => {
+
+    let selectedNumber = parseFloat(calcDisplay.textContent);
+    let selectedOperation = e.target.id;
+
+    if (calcStateObj.selectedOperation) {
+        
+        storeNumber(calcStateObj, selectedNumber);
+        calculateOperation(calcStateObj);
+
+        storeOperation(calcStateObj, selectedOperation);
+
+        calcDisplay.textContent = calcStateObj.calculationResult;
+        
+    } else {
+        
+        storeNumber(calcStateObj, selectedNumber);
+        storeOperation(calcStateObj, selectedOperation);
+
+    }
+
+    calcStateObj.isAfterOperation = true;
+   
+}
+
+operatorNodeList.forEach( operator => {
+    
+    operator.addEventListener("click", storeInput)
+
+} )
+
